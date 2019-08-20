71ca846aa94d1080a5d51f7cc02906d7383047c8
ofirle <41347272+ofirle@users.noreply.github.com>
2019-08-20 19:33:03 +0300

    ofir

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Hector");
+        primaryStage.setTitle("Kan Pishuto Rut AiSher");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
